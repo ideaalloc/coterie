e99514440300e00a178d195886e48c5a95daac15
lvchao <billcc.lv@gmail.com>
2014-12-07 00:21:58 +0800

    Bill configured jdbc connection.

---
+++ repository/src/main/resources/sqlscripts/init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`coterie` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
+CREATE USER 'coterie'@'%' IDENTIFIED BY 'servicerequest';
+GRANT ALL PRIVILEGES ON coterie.* TO 'coterie'@'%';
+-- UPDATE mysql.user SET Password=PASSWORD('servicerequest') where USER='coterie';
+FLUSH PRIVILEGES;
